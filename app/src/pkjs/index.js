fdbe3e1bd321425f68b7168819822b960e2ab7ae
Claudio <itsthered1@users.noreply.github.com>
2020-09-16 09:57:49 -0700

    Added gamesmenu / scores
    Also rebuilt

--- app/src/pkjs/index.js
+++ app/src/pkjs/index.js
@@ -36,15 +36,17 @@ leagues.show();
 
 //this function receives 'sport' which is a string variable. It should be the title of a menu item like Hockey
 function getSportsData(sport) {
-  //lets make a blank varible called APIURL. This variable will be filled later with a real URL pointing to the appropriate API
   var APIURL = '';
-
-  //now let's check to see which sport the user selected
   if (sport == "Football") {
-    //if the sport is football, we set the API URL to the football API.
     APIURL = 'http://site.api.espn.com/apis/site/v2/sports/football/nfl/scoreboard';
   } else if (sport == "Hockey") {
     APIURL = 'http://site.api.espn.com/apis/site/v2/sports/hockey/nhl/scoreboard';
+  } else if (sport == "Baseball") {
+    APIURL = 'http://site.api.espn.com/apis/site/v2/sports/baseball/mlb/scoreboard';
+  } else if (sport == "Basketball") {
+    APIURL = 'http://site.api.espn.com/apis/site/v2/sports/basketball/nba/scoreboard';
+  } else if (sport == "Soccer") {
+    APIURL = 'http://site.api.espn.com/apis/site/v2/sports/soccer/:league/scoreboard';
     //if the sport is hockey, we set the API URL to the hockey API.
     //find APIs here: https://gist.github.com/akeaswaran/b48b02f1c94f873c6655e7129910fc3b
 
@@ -66,15 +68,37 @@ function getSportsData(sport) {
               var sportsData = JSON.parse(req.responseText); //<-right here
               //console.log(JSON.stringify(sportsData)); // let's log it to console to see what came from the API
               var games = sportsData.events; // after exploring a bit I found that games are actually an array inside this events object
-              for (var i = 0; i < games.length; i++){
-                //this will loop through each game and show it's short name in the console
-                console.log(games[i].shortName);
-                //there is a lot more data to explore but we can get to that later
+              // for (var i = 0; i < games.length; i++){
+              //this will loop through each game and show it's short name in the console
+              //console.log(games[i].shortName);
+              //there is a lot more data to explore but we can get to that later
+              showGamesMenu(sport, games);
               }
             }
           }
-      }
       req.send();
+}
 
+function showGamesMenu(sport, games){
+  var gameMenuItems = [];
+  for (var i = 0; i < games.length; i++){
+   var gameMenuItem = {
+     title: games[i].shortName,
+     subtitle: games[i].competitions[0].competitors[1].score + " to " + games[i].competitions[0].competitors[0].score
+   }
+   gameMenuItems.push(gameMenuItem);
+  }
+  var gameMenu = new UI.Menu({
+    backgroundColor: 'white',
+    textColor: 'black',
+    highlightBackgroundColor: 'vivid-cerulean',
+    highlightTextColor: 'black',
+    sections: [{
+      title: sport,
+      items: gameMenuItems
+    }]
+  });
+
+  gameMenu.show();
 
 }
