7be2ab7af8f35799c5278478c5f446728b596c90
pauleeeeee <paul.embleton@gmail.com>
2020-09-15 18:40:34 -0600

    added games menu and scores

--- app/src/pkjs/index.js
+++ app/src/pkjs/index.js
@@ -48,7 +48,7 @@ function getSportsData(sport) {
   } else if (sport == "Hockey") {
     //if the sport is hockey, we set the API URL to the hockey API.
     //find APIs here: https://gist.github.com/akeaswaran/b48b02f1c94f873c6655e7129910fc3b
-
+    APIURL = 'http://site.api.espn.com/apis/site/v2/sports/hockey/nhl/scoreboard';
   } else {
     console.log('need to add support for more sports!');
   }
@@ -67,15 +67,38 @@ function getSportsData(sport) {
               var sportsData = JSON.parse(req.responseText); //<-right here
               //console.log(JSON.stringify(sportsData)); // let's log it to console to see what came from the API
               var games = sportsData.events; // after exploring a bit I found that games are actually an array inside this events object
-              for (var i = 0; i < games.length; i++){
-                //this will loop through each game and show it's short name in the console
-                console.log(games[i].shortName);
-                //there is a lot more data to explore but we can get to that later
-              }
+              // for (var i = 0; i < games.length; i++){
+              //   //this will loop through each game and show it's short name in the console
+              //   console.log(games[i].shortName);
+              //   //there is a lot more data to explore but we can get to that later
+              // }
+              showGamesMenu(sport, games);
             }
           }
       }
-      req.send();
+  req.send();
+}
 
+function showGamesMenu(sport, games){
+  var gameMenuItems = [];
+  for (var i = 0; i < games.length; i++){
+   var gameMenuItem = {
+     title: games[i].shortName,
+     subtitle: games[i].competitions[0].competitors[1].score + " to " + games[i].competitions[0].competitors[0].score
+   }
+   gameMenuItems.push(gameMenuItem);
+  }
+  var gameMenu = new UI.Menu({
+    backgroundColor: 'white',
+    textColor: 'black',
+    highlightBackgroundColor: 'vivid-cerulean',
+    highlightTextColor: 'black',
+    sections: [{
+      title: sport,
+      items: gameMenuItems
+    }]
+  });
 
+  gameMenu.show();
+  
 }
